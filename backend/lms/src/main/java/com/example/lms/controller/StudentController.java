7fbb3e2e3fc03ade98f8dcef536495e4f8ecf6fc
Why-Anubhav <anubhavyadav9204@gmail.com>
2026-06-05 12:02:33 +0530

    Day 3 collection and generics

--- backend/lms/src/main/java/com/example/lms/controller/StudentController.java
+++ backend/lms/src/main/java/com/example/lms/controller/StudentController.java
@@ -3,18 +3,30 @@ package com.example.lms.controller; // create package for global access
 import com.example.lms.model.Student;  // import according to model.name package
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController               //allows use of rest APIs 
 @RequestMapping("/student")  // maps request 
 @CrossOrigin(origins ="http://localhost:5173") // connect with frontend
 public class StudentController {
 
 @GetMapping
-    public Student getStudent() {
+    public ArrayList<Student> getStudents() {
+        ArrayList<Student> students = new ArrayList<>();
 
-        return new Student(
-                1,
-                "Tushar",
-                "MCA"
+        students.add(
+                new Student(1, "Tushar", "MCA")
         );
+
+        students.add(
+                new Student(2, "Rahul", "MCA")
+        );
+
+        students.add(
+                new Student(3, "Aman", "BTech")
+        );
+
+        return students;
+        
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function App(){
     // {"id":1,"name":"Tushar","course":"MCA"}
-    const [ students,setStudent] = useState();
+    const [ students,setStudent] = useState([]);
 
 const getStudentData = async() => {
     const response = await fetch("http://localhost:8080/student");
@@ -16,10 +16,15 @@ const getStudentData = async() => {
             <button onClick={getStudentData}>
                 Get Student Data
             </button>
-            <h2>(students.name)</h2>
-            <h2>(students.course)</h2>
-            <h2>(students.id)</h2>
 
+            <ul>
+                {students.map((student) =>(
+                    <li key={student.id}>
+                        {student.name}
+                        {student.course}
+                    </li>
+                ))}
+            </ul>
 
         </div>
     )
